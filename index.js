6abd4829da26b8fca27ae2eb1cd6078a2bb13959
gwagsi <gwagsiglenn@gmail.com>
2023-05-21 22:49:02 +0100

    add and diplay new books

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let myLibrary = [2];
-let books = document.getElementById(books);
+let myLibrary = [];
+//let books = document.getElementById(books);
 
 function Book(title, author, pages) {
   // the constructor...
@@ -16,26 +16,29 @@ function addBookToLibrary(title, author, pages) {
 
 function getValue() {
   console.log(myLibrary);
-  console.log("Hello");
   title = document.getElementById("title").value;
-  console.log(title);
   author = document.getElementById("author").value;
   pages = document.getElementById("pages").value;
   addBookToLibrary(title, author, pages);
-  console.log(myLibrary[3].title);
 }
-console.log("here are me");
+
 function displayBooks() {
-  console.log("we are here");
-  table = document.getElementById("table");
+  const table = document.getElementById("table");
+  const tableHeading = table.firstElementChild;
+  table.innerHTML = "";
+  table.appendChild(tableHeading);
   myLibrary.forEach((book) => {
-    let tr = document.createElement(tr);
-    for (key in book) {
-      let th = document.createElement(th);
-      th.innerText = book(key);
-      tr.appenChild(th);
+    let tr = document.createElement("tr");
+    for (const key in book) {
+      console.log("we are here2");
+      let th = document.createElement("th");
+      console.log(`${key}: ${book[key]}`);
+      th.innerText = book[key];
+      console.log(th);
+      tr.appendChild(th);
+      console.log(tr);
     }
-    table.appenChild(tr);
+    console.log(table);
+    table.appendChild(tr);
   });
 }
-displayBooks();
